198b617d009f6a48587b061b7617e1c6574470ca
Henrik Andersen <han@rts.dk>
2022-10-13 14:52:38 +0200

    fix prevent mobile scroll

--- js/snake-game.js
+++ js/snake-game.js
@@ -2,4 +2,6 @@ import SnakeBoard from './snake-board.js';
 
 let board = new SnakeBoard();
 
+['touchstart', 'touchmove', 'scroll'].forEach(eventType => document.addEventListener(eventType, event => event.preventDefault()));
+
 document.body.appendChild(board.element);
